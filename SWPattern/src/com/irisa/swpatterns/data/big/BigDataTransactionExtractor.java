a09ca0ae58657ddec68462d5739801ae64479d6d
pmaillot <pierre.maillot@irisa.fr>
2017-11-07 10:34:56 +0100

    Proper parser error handling, uris with space still a problem

--- SWPattern/src/com/irisa/swpatterns/data/big/BigDataTransactionExtractor.java
+++ SWPattern/src/com/irisa/swpatterns/data/big/BigDataTransactionExtractor.java
@@ -1,11 +1,13 @@
 package com.irisa.swpatterns.data.big;
 
+import java.lang.Thread.UncaughtExceptionHandler;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.apache.jena.ext.com.google.common.util.concurrent.UncaughtExceptionHandlers;
 import org.apache.jena.graph.Triple;
 import org.apache.jena.rdf.model.AnonId;
 import org.apache.jena.rdf.model.Model;
@@ -13,9 +15,14 @@ import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFParser;
+import org.apache.jena.riot.RiotException;
+import org.apache.jena.riot.RiotParseException;
 import org.apache.jena.riot.lang.PipedRDFIterator;
 import org.apache.jena.riot.lang.PipedTriplesStream;
+import org.apache.jena.riot.system.ErrorHandler;
+import org.apache.jena.riot.system.ErrorHandlerFactory;
 import org.apache.jena.vocabulary.RDF;
 import org.apache.log4j.Logger;
 
@@ -31,35 +38,35 @@ import com.irisa.swpatterns.data.RDFPatternComponent.Type;
 import com.irisa.swpatterns.data.RDFPatternResource;
 
 public class BigDataTransactionExtractor {
-	
+
 	private static Logger logger = Logger.getLogger(BigDataTransactionExtractor.class);
-	
+
 	private HashSet<Resource> _individuals = new HashSet<Resource>();
 	private UtilOntology _onto = new UtilOntology();
 
 	private boolean _noTypeBool = false;
 	private boolean _noInBool = false;
 	private boolean _noOutBool = false;	
-	
+
 	private Neighborhood _neighborLevel = Neighborhood.PropertyAndType;
-	
+
 	private HashMap<Resource, LabeledTransaction> _buildingTransactionsTypeItems = new HashMap<Resource, LabeledTransaction>(); // TYPE items per resource
 	private HashMap<Resource, LabeledTransaction> _buildingTransactionsPropertyItems = new HashMap<Resource, LabeledTransaction>(); // PROPERTY items per resource
-	
+
 	private HashMap<Resource, LabeledTransaction> _buildingtransactionsPropertyClassItems = new HashMap<Resource, LabeledTransaction>();
 
 	public boolean isKnownIndividual(Resource indiv) {
 		return _individuals.contains(indiv);
 	}
-	
+
 	public void addKnownIndividual(Resource indiv) {
 		_individuals.add(indiv);
 	}
-	
+
 	public void setNeighborLevel(Neighborhood level) {
 		this._neighborLevel = level;
 	}
-	
+
 	public Neighborhood getNeighborLevel() {
 		return this._neighborLevel;
 	}
@@ -87,172 +94,202 @@ public class BigDataTransactionExtractor {
 	public void setNoOutTriples(boolean noOutBool) {
 		this._noOutBool = noOutBool;
 	}
-	
+
+	private static Thread bigdataParserThread(String filename, PipedTriplesStream dataStream) {
+		Thread parser=new Thread(){
+			@Override public void run(){
+				try {
+					RDFParser.source(filename)
+					.checking(false) // SHOULD NOT stop at malformed IRIs, but does anyway when they contain spaces...
+					.errorHandler(ErrorHandlerFactory.errorHandlerNoWarnings)
+					.errorHandler(new ErrorHandler() {
+						private Logger rdfParserLogger = Logger.getLogger(RDFParser.class);
+						
+						@Override
+						public void warning(String arg0, long arg1, long arg2) {
+							rdfParserLogger.warn("line: " + arg1 + " col: " + arg2 + ", " + arg0);
+						}
+						@Override
+						public void fatal(String arg0, long arg1, long arg2) {
+							warning(arg0, arg1, arg2);
+						}
+						@Override
+						public void error(String arg0, long arg1, long arg2) {
+							warning(arg0, arg1, arg2);
+						}
+					})
+					.parse(dataStream);
+				} catch(Exception e) {
+					logger.error("Unhandled exception during parsing, Dataset probably badly formated: ", e);
+					throw e;
+				}
+			}};
+
+			return parser;
+	}
+
 	public ItemsetSet extractTransactionsFromFile(String filename) {
 		logger.debug("big data loading START");
-		
+
 		logger.debug("Jena loading ...");
-//		// First, line by line, fill the indexes
+		// First, line by line, fill the indexes
 		Model model = ModelFactory.createDefaultModel(); 
 		PipedRDFIterator<Triple> dataIt = new PipedRDFIterator<Triple>();
 		PipedTriplesStream dataSteam = new PipedTriplesStream(dataIt);
 		ExecutorService executor=Executors.newSingleThreadExecutor();
+
 		int nbtriples = 1;
 		int nbMaxtriples = 0;
 		try {
-		Thread parser=new Thread(){
-			@Override public void run(){
-				RDFParser.source(filename).parse(dataSteam);
-		}};
-		executor.submit(parser);
-		
-		logger.debug("Jena loading ended");
-		
+			Thread parser = bigdataParserThread(filename, dataSteam);
+			executor.submit(parser);
+
+			logger.debug("Jena loading ended");
+
 			// Filling the indexes
-		while(dataIt.hasNext()) {
-			try {
-			Triple stat = dataIt.next();
-			Property prop = null;
-			Resource subj = null;
-			RDFNode obj = null;
-			if(stat.getSubject() != null && stat.getSubject().getURI() != null) {
-				subj = model.createResource(stat.getSubject().getURI());
-			}
-			if(stat.getPredicate() != null && stat.getPredicate().getURI() != null) {
-				prop = model.createProperty(stat.getPredicate().getURI());
-			}
-			if(stat.getObject() != null) {
-				if(stat.getObject().isLiteral()) {
-					obj = model.createTypedLiteral(stat.getObject().getLiteralValue(), stat.getObject().getLiteralDatatype());
-				} else if(stat.getObject().isURI()) {
-					obj = model.createResource(stat.getObject().getURI());
-				} else if(stat.getObject().isBlank()) {
-					obj = model.createResource(new AnonId(stat.getObject().getBlankNodeId())); 
-				}
-			}
-			
-			if(subj != null && prop != null && obj != null) {
-				if(prop.equals(RDF.type)) { // Instantiation triple
-					if(! (obj.isLiteral())) { // checking basic RDF rule respect
-						Resource objRes = obj.asResource();
-						RDFPatternResource compoType = AttributeIndex.getInstance().getComponent(objRes, Type.TYPE);
-						addComponentToIndexes(subj, compoType);
+			while(dataIt.hasNext()) {
+				try {
+					Triple stat = dataIt.next();
+					Property prop = null;
+					Resource subj = null;
+					RDFNode obj = null;
+					if(stat.getSubject() != null && stat.getSubject().getURI() != null) {
+						subj = model.createResource(stat.getSubject().getURI());
 					}
-				} else if(! _onto.isOntologyPropertyVocabulary(prop) && ! _onto.isOntologyClassVocabulary(subj)) { // property and subject not ontology stuff
-				
-					RDFPatternResource compoPropOut = AttributeIndex.getInstance().getComponent(prop, Type.OUT_PROPERTY);
-					addComponentToIndexes(subj, compoPropOut);
-					
-					if(! obj.isLiteral()){ // Object is not a literal 
-						Resource objRes = obj.asResource();
-						if(! _onto.isOntologyClassVocabulary(objRes)) { // Object is not Ontology stuff
-							RDFPatternResource compoPropIn = AttributeIndex.getInstance().getComponent(prop, Type.IN_PROPERTY);
-							addComponentToIndexes(objRes, compoPropIn);
+					if(stat.getPredicate() != null && stat.getPredicate().getURI() != null) {
+						prop = model.createProperty(stat.getPredicate().getURI());
+					}
+					if(stat.getObject() != null) {
+						if(stat.getObject().isLiteral()) {
+							obj = model.createTypedLiteral(stat.getObject().getLiteralValue(), stat.getObject().getLiteralDatatype());
+						} else if(stat.getObject().isURI()) {
+							obj = model.createResource(stat.getObject().getURI());
+						} else if(stat.getObject().isBlank()) {
+							obj = model.createResource(new AnonId(stat.getObject().getBlankNodeId())); 
 						}
 					}
+
+					if(subj != null && prop != null && obj != null) {
+						logger.trace("triple n° " + nbtriples + " read: " + subj + " " + prop + " " + obj);
+						if(prop.equals(RDF.type)) { // Instantiation triple
+							if(! (obj.isLiteral())) { // checking basic RDF rule respect
+								Resource objRes = obj.asResource();
+								RDFPatternResource compoType = AttributeIndex.getInstance().getComponent(objRes, Type.TYPE);
+								addComponentToIndexes(subj, compoType);
+							}
+						} else if(! _onto.isOntologyPropertyVocabulary(prop) && ! _onto.isOntologyClassVocabulary(subj)) { // property and subject not ontology stuff
+
+							RDFPatternResource compoPropOut = AttributeIndex.getInstance().getComponent(prop, Type.OUT_PROPERTY);
+							addComponentToIndexes(subj, compoPropOut);
+
+							if(! obj.isLiteral()){ // Object is not a literal 
+								Resource objRes = obj.asResource();
+								if(! _onto.isOntologyClassVocabulary(objRes)) { // Object is not Ontology stuff
+									RDFPatternResource compoPropIn = AttributeIndex.getInstance().getComponent(prop, Type.IN_PROPERTY);
+									addComponentToIndexes(objRes, compoPropIn);
+								}
+							}
+						}
+					}
+					if(nbtriples % 1000000 == 0) {
+						logger.debug("Reaching " + nbtriples + " triples, loading...");
+					}
+					nbtriples++;
+					nbMaxtriples++;
+					Thread.sleep(0);
+				} catch(Exception e) { // Catching the neurotic Jena parser exceptions
+					logger.error("Exception during this line treatment: ", e);
 				}
 			}
-			if(nbtriples % 1000000 == 0) {
-				logger.debug("Reaching " + nbtriples + " triples, loading...");
-			}
-			nbtriples++;
-			nbMaxtriples++;
-			Thread.sleep(0);
-			} catch(Exception e) { // Catching the neurotic Jena parser exceptions
-				logger.error("Exception during this line treatment: ", e);
-			}
-		}
-		logger.debug("Property based items built");
+			logger.debug("Property based items built");
 		} finally {
 			executor.shutdown();
+			dataIt.close();
 		}
 		logger.debug("End of first reading");
-		
+
 		if(this.getNeighborLevel() == Neighborhood.PropertyAndType) {
 			logger.debug("Second reading of the file fo the property-class conversion");
 			// First, line by line, fill the indexes
 			PipedRDFIterator<Triple> dataItSecond = new PipedRDFIterator<Triple>();
 			PipedTriplesStream dataSteamSecond = new PipedTriplesStream(dataItSecond);
 			ExecutorService executorSecond=Executors.newSingleThreadExecutor();
-			
+
 			try {
-			Thread parserSecond=new Thread(){
-				@Override public void run(){
-					RDFParser.source(filename).parse(dataSteamSecond);
-			}};
-			executorSecond.submit(parserSecond);
-			
+				Thread parserSecond = bigdataParserThread(filename, dataSteamSecond);
+				executorSecond.submit(parserSecond);
+
 				// Filling the indexes
-			nbtriples = 1;
-			while(dataItSecond.hasNext()) {
-				try {
-				Triple stat = dataItSecond.next();
-				Property prop = null;
-				Resource subj = null;
-				RDFNode obj = null;
-				if(stat.getSubject() != null && stat.getSubject().getURI() != null) {
-					subj = model.createResource(stat.getSubject().getURI());
-				}
-				if(stat.getPredicate() != null && stat.getPredicate().getURI() != null) {
-					prop = model.createProperty(stat.getPredicate().getURI());
-				}
-				if(stat.getObject() != null) {
-					if(stat.getObject().isLiteral()) {
-						obj = model.createTypedLiteral(stat.getObject().getLiteralValue(), stat.getObject().getLiteralDatatype());
-					} else if(stat.getObject().isURI()) {
-						obj = model.createResource(stat.getObject().getURI());
-					} else if(stat.getObject().isBlank()) {
-						obj = model.createResource(new AnonId(stat.getObject().getBlankNodeId())); 
-					}
-				}
-				
-				if(subj != null 
-						&& prop != null 
-						&& obj != null 
-						&& obj.isResource() 
-						&& ! _onto.isOntologyPropertyVocabulary(prop) 
-						&& ! _onto.isOntologyClassVocabulary(obj.asResource())) {
-					if(this._buildingTransactionsTypeItems.get(obj) != null) {
-						Iterator<RDFPatternComponent> itObjType = this._buildingTransactionsTypeItems.get(obj).iterator();
-						while(itObjType.hasNext()) {
-							RDFPatternComponent compoObjType = itObjType.next();
-							Resource objType = ((RDFPatternResource) compoObjType).getResource();
-							
-							RDFPatternComponent compoOut = AttributeIndex.getInstance().getComponent(prop, objType, Type.OUT_NEIGHBOUR_TYPE);
-							addComponentToIndexes(subj, compoOut);
+				nbtriples = 1;
+				while(dataItSecond.hasNext()) {
+					try {
+						Triple stat = dataItSecond.next();
+						Property prop = null;
+						Resource subj = null;
+						RDFNode obj = null;
+						if(stat.getSubject() != null && stat.getSubject().getURI() != null) {
+							subj = model.createResource(stat.getSubject().getURI());
 						}
+						if(stat.getPredicate() != null && stat.getPredicate().getURI() != null) {
+							prop = model.createProperty(stat.getPredicate().getURI());
+						}
+						if(stat.getObject() != null) {
+							if(stat.getObject().isLiteral()) {
+								obj = model.createTypedLiteral(stat.getObject().getLiteralValue(), stat.getObject().getLiteralDatatype());
+							} else if(stat.getObject().isURI()) {
+								obj = model.createResource(stat.getObject().getURI());
+							} else if(stat.getObject().isBlank()) {
+								obj = model.createResource(new AnonId(stat.getObject().getBlankNodeId())); 
+							}
+						}
+
+						if(subj != null 
+								&& prop != null 
+								&& obj != null 
+								&& obj.isResource() 
+								&& ! _onto.isOntologyPropertyVocabulary(prop) 
+								&& ! _onto.isOntologyClassVocabulary(obj.asResource())) {
+							if(this._buildingTransactionsTypeItems.get(obj) != null) {
+								Iterator<RDFPatternComponent> itObjType = this._buildingTransactionsTypeItems.get(obj).iterator();
+								while(itObjType.hasNext()) {
+									RDFPatternComponent compoObjType = itObjType.next();
+									Resource objType = ((RDFPatternResource) compoObjType).getResource();
+
+									RDFPatternComponent compoOut = AttributeIndex.getInstance().getComponent(prop, objType, Type.OUT_NEIGHBOUR_TYPE);
+									addComponentToIndexes(subj, compoOut);
+								}
+							}
+
+							if(this._buildingTransactionsTypeItems.get(subj) != null) {
+								Iterator<RDFPatternComponent> itSubjType = this._buildingTransactionsTypeItems.get(subj).iterator();
+								while(itSubjType.hasNext()) {
+									RDFPatternComponent compoSubjType = itSubjType.next();
+									Resource subjType = ((RDFPatternResource) compoSubjType).getResource();
+
+									RDFPatternComponent compoIn = AttributeIndex.getInstance().getComponent(prop, subjType, Type.IN_NEIGHBOUR_TYPE);
+									addComponentToIndexes(obj.asResource(), compoIn);
+								}	
+							}
+						}
+						if(nbtriples % 1000000 == 0) {
+							logger.debug("Reaching " + nbtriples + " triples over " + nbMaxtriples + ", loading...");
+						}
+						nbtriples++;
+						Thread.sleep(0);
+					} catch(Exception e) { // Catching the neurotic Jena parser exceptions
+						logger.error("Exception during this line treatment: ", e);
 					}
-					
-					if(this._buildingTransactionsTypeItems.get(subj) != null) {
-						Iterator<RDFPatternComponent> itSubjType = this._buildingTransactionsTypeItems.get(subj).iterator();
-						while(itSubjType.hasNext()) {
-							RDFPatternComponent compoSubjType = itSubjType.next();
-							Resource subjType = ((RDFPatternResource) compoSubjType).getResource();
-							
-							RDFPatternComponent compoIn = AttributeIndex.getInstance().getComponent(prop, subjType, Type.IN_NEIGHBOUR_TYPE);
-							addComponentToIndexes(obj.asResource(), compoIn);
-						}	
-					}
 				}
-				if(nbtriples % 1000000 == 0) {
-					logger.debug("Reaching " + nbtriples + " triples over " + nbMaxtriples + ", loading...");
-				}
-				nbtriples++;
-				Thread.sleep(0);
-				} catch(Exception e) { // Catching the neurotic Jena parser exceptions
-					logger.error("Exception during this line treatment: ", e);
-				}
-			}
-			logger.debug("End of second reading");
+				logger.debug("End of second reading");
 			} finally {
+				dataItSecond.close();
 				executorSecond.shutdown();
 			}
 			logger.debug("Property-class based items built");
 		}
 		model.close();
-		
-//		
-		
+
+
 		logger.debug("Union of all tmp transactions for " + this._individuals.size() + " individuals");
 		// Union of the transactions
 		ItemsetSet result = new ItemsetSet();
@@ -260,7 +297,7 @@ public class BigDataTransactionExtractor {
 		int nbtreatedIndiv = 1;
 		while(itIndiv.hasNext()) {
 			Resource indiv = itIndiv.next();
-			
+
 			KItemset indivTrans = new KItemset();
 			indivTrans.setLabel(indiv.toString());
 			if(this._buildingTransactionsTypeItems.containsKey(indiv)) {
@@ -275,21 +312,21 @@ public class BigDataTransactionExtractor {
 					this._buildingtransactionsPropertyClassItems.remove(indiv);
 				}
 			}
-			
+
 			if(! indivTrans.isEmpty()) {
 				result.add(indivTrans);
 			}
-			
+
 			if(nbtreatedIndiv % 100000 == 0) {
 				logger.debug("Individual n°" + nbtreatedIndiv);
 			}
 			nbtreatedIndiv++;
 		}
 		logger.debug("All transactions united, " + result.size() + " transactions for " + AttributeIndex.getInstance().size() + " attributes");
-		
+
 		return result;
 	}
-	
+
 	private void addComponentToIndexes(Resource res, RDFPatternComponent compo) {
 		logger.trace("Adding component " + compo + " for resource " + res);
 		if(!this._individuals.contains(res)) {
@@ -302,23 +339,23 @@ public class BigDataTransactionExtractor {
 				this._buildingTransactionsPropertyItems.put(res, new LabeledTransaction(res));
 			}
 			this._buildingTransactionsPropertyItems.get(res).add(compo);
-		break;
+			break;
 		case TYPE:
 			if(! this._buildingTransactionsTypeItems.containsKey(res)) { 
 				this._buildingTransactionsTypeItems.put(res, new LabeledTransaction(res));
 			}
 			this._buildingTransactionsTypeItems.get(res).add(compo);
-		break;
+			break;
 		case OUT_NEIGHBOUR_TYPE:
 		case IN_NEIGHBOUR_TYPE:
 			if(! this._buildingtransactionsPropertyClassItems.containsKey(res)) {
 				this._buildingtransactionsPropertyClassItems.put(res, new LabeledTransaction(res));
 			}
 			this._buildingtransactionsPropertyClassItems.get(res).add(compo);
-		break;
+			break;
 		default:
 			throw new LogicException("Unexpected element \""+ compo +"\" to add to the indexes for resource " + res );
 		}
 	}
-	
+
 }
